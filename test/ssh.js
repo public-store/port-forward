6be45203ba95f17b1477e3306eb55a5011b475d3
Lellansin <lellansin@gmail.com>
2015-01-30 15:06:04 +0800

    add ssh transport test

---
+++ test/ssh.js
@@ -0,0 +1,11 @@
+var Transport = require('../');
+
+var transportor = new Transport({
+    client: {
+        port: 90
+    },
+    server: {
+        host: '210.14.141.253',
+        port: 22
+    }
+});
